3978a711ed25eb7dc842428f8c35612b93cb8852
Guerreiro <igor.silva@xbrain.com.br>
2024-01-20 15:25:47 -0300

    refactor

--- src/main/java/com/igorbavand/vendasapi/config/SecurityConfigurations.java
+++ src/main/java/com/igorbavand/vendasapi/config/SecurityConfigurations.java
@@ -7,6 +7,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -23,10 +24,10 @@ public class SecurityConfigurations {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 
-        String[] permitAll = {"/swagger-ui/**", "/v3/api-docs/**", "/api/auth/login", "/api/auth/register"};
+        String[] permitAll = {"/swagger-ui/**", "/v3/api-docs/**", "/api/auth/login", "/api/auth/register", "/public/**", "/api/venda/webhook-listener"};
 
         return httpSecurity
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(permitAll).permitAll()
